b6fbe11a80a7439e4bbc4d0d340cd901dad31688
Rae Stanton <raestanton93@gmail.com>
2023-08-28 09:50:36 -0400

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    function handleCountdown() {
+      setTimeRemaining(prevTime => {
+        if (prevTime > 1) {
+          setTimeout(handleCountdown, 1000);
+          return prevTime - 1;
+        } else {
+          onAnswered(false);
+          return 10;
+        }
+      });
+    }
+
+    const initialTimeout = setTimeout(handleCountdown, 1000);
+    return () => {
+      clearTimeout(initialTimeout);
+    };
+  }, [question.id]);  // Only re-run effect if the question changes
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
